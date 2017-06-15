743c0b6e6c6386ec87dadc5d742641288b8e4c5a
Rick Lewis <ejlewis2@illinois.edu>
2017-06-15 10:30:38 -0500

    test commit 3

--- src/map.js
+++ src/map.js
@@ -111,7 +111,7 @@ function mapReadImageMetadata(image) {
 // ----------------------------------------------------------------
 
 function mapPopulate() {
-    console.log("Map Center: " + mapCenter);
+    console.log("Map Center: " + mapCenter.lat + ", " + mapCenter.lng);
     map = new google.maps.Map(document.getElementById(MAP_DIV_ID), {zoom: 8, center: mapCenter});
     for (var markerIndex = 0; markerIndex < mapMarkers.length; markerIndex++) {
         mapMarkers.push(new google.maps.Marker({position: mapMarkers[markerIndex], map: map}));
